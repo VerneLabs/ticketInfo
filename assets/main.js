a0c9aba16ca43cd699bc3f23ceb6a308bdd62875
magiobus <aalbertobus@gmail.com>
2017-11-22 17:50:44 -0600

    Handlebars

--- assets/main.js
+++ assets/main.js
@@ -4,14 +4,9 @@ client.invoke('resize', { width: '100%', height: '200px' });
 client.get('ticket').then(function(response){
   var userId = response.ticket.requester.id
   requestUserInfo(client, userId).then(function(response){
-    var userData = {
-      "name": response.user.name,
-      "email": response.user.email,
-      "role": response.user.role,
-      "time_zone": response.user.time_zone,
-      "created_at": response.user.created_at
-    }
-    console.log("userData =>", userData)
+    showInfo(response);
+  }).catch(function(error){
+    showError()
   })
 })
 
@@ -22,4 +17,30 @@ function requestUserInfo(client, id) {
     dataType: 'json',
   };
   return client.request(settings)
+}
+
+function showInfo(response){
+  var userData = {
+    "name": response.user.name,
+    "email": response.user.email,
+    "role": response.user.role,
+    "time_zone": response.user.time_zone,
+    "created_at": response.user.created_at
+  }
+
+  var source = $("#requester-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(userData);
+  $("#content").html(html);
+}
+
+function showError() {
+  var error_data = {
+    'status': 404,
+    'statusText': 'Not found'
+  };
+  var source = $("#error-template").html();
+  var template = Handlebars.compile(source);
+  var html = template(error_data);
+  $("#content").html(html);
 }
